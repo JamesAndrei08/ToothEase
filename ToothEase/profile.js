072e612cdd4acf3864a1a98f5332f96615cbae95
JamesAndrei08 <160197069+JamesAndrei08@users.noreply.github.com>
2024-09-27 23:44:55 +0800

    Delete ToothEase directory

--- ToothEase/profile.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Toggle profile dropdown
-    document.querySelector('.profile').addEventListener('click', function() {
-        this.classList.toggle('active');
-    });
-});
